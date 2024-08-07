67170c83ea601ad9b32c52b1c9654590eaa05495
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-07 11:17:26 +0530

    Add success notify and set error Typo

--- src/util/notify.js
+++ src/util/notify.js
@@ -1,6 +1,19 @@
 import { toast } from 'react-toastify';
-export const notify = (message)=>{
+export const ErrorNotify = (message)=>{
     toast.error(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+    });
+}
+
+export const SuccessNotify = (message)=>{
+    toast.success(message, {
         position: "top-right",
         autoClose: 5000,
         hideProgressBar: false,
