75279a845212707f6dfec9b85a831cd6c9d68d7d
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-08 14:23:58 +0530

    Add Card Action - Delete and Edit Buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,13 @@ import { validate } from "../helper/homeValidate";
 import { useState, useEffect } from "react";
 import { ErrorNotify, SuccessNotify } from "../util/notify";
 const getColor = (index) => {
-  if (index % 3 === 0) { //0,3,6,9,12
+  if (index % 3 === 0) {
+    //0,3,6,9,12
     return "red";
-  } else if (index % 3 === 1) { //1,4,,7,10
+  } else if (index % 3 === 1) {
+    //1,4,,7,10
     return "yellow";
-  } else { 
+  } else {
     return "blue";
   }
 };
@@ -152,6 +154,38 @@ export default function Home() {
                 <span className="timestamp">
                   {frmatingDate(item.createdAt)}
                 </span>
+                <div className="card-action">
+                  <button aria-labelledby="Delete">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      x="0px"
+                      y="0px"
+                      width="100"
+                      height="100"
+                      viewBox="0 0 72 72"
+                      fill="#e74c3c"
+                    >
+                      <path  d="M 32 13 C 30.895 13 30 13.895 30 15 L 30 16 L 17 16 C 14.791 16 13 17.791 13 20 C 13 21.973645 14.432361 23.602634 16.3125 23.929688 L 18.707031 52.664062 C 19.053031 56.811062 22.520641 60 26.681641 60 L 45.318359 60 C 49.479359 60 52.945969 56.811062 53.292969 52.664062 L 55.6875 23.929688 C 57.567639 23.602634 59 21.973645 59 20 C 59 17.791 57.209 16 55 16 L 42 16 L 42 15 C 42 13.895 41.105 13 40 13 L 32 13 z M 24.347656 24 L 47.652344 24 L 45.396484 51.082031 C 45.352484 51.600031 44.918438 52 44.398438 52 L 27.601562 52 C 27.081562 52 26.647469 51.600031 26.605469 51.082031 L 24.347656 24 z"></path>
+                    </svg>
+                  </button>
+                  <button aria-labelledby="Edit">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      x="0px"
+                      y="0px"
+                      width="100"
+                      height="100"
+                      fill="#3498db"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M18.4,4.4l1.2,1.2L6.2,19H5v-1.2L18.4,4.4 M18.4,2c-0.3,0-0.5,0.1-0.7,0.3L3,17v4h4L21.7,6.3c0.4-0.4,0.4-1,0-1.4l-2.6-2.6 C18.9,2.1,18.7,2,18.4,2L18.4,2z"></path>
+                      <path
+                        d="M15.8 4.3H17.8V9.2H15.8z"
+                        transform="rotate(-45.001 16.75 6.75)"
+                      ></path>
+                    </svg>
+                  </button>
+                </div>
               </div>
             ))
           ) : (
