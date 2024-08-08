9a71fb588a8992a21a32fe62d69e78d643884e93
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-08 10:52:29 +0530

    Note validation

---
+++ src/helper/homeValidate.js
@@ -0,0 +1,18 @@
+export const validate = (formData) => {
+  const newFormError = {};
+  if (!formData.title) {
+    newFormError.title = "Title is required field 😪";
+  } else if (formData.title.length < 20) {
+    newFormError.title = "The title is too short (at least 20 characters) 🩳";
+  } else if (formData.title.length > 30) {
+    newFormError.title = "The title is too long (maximum of 30 characters)🪘";
+  }
+  if (!formData.description) {
+    newFormError.description = "Description is required 😪";
+  } else if (formData.description.length < 150) {
+    newFormError.description = "The description is too short (at least 150 characters) 🩳";
+  } else if (formData.description.length > 300) {
+    newFormError.description = "The description is too long (maximum of 300 characters) 🪘";
+  }
+  return newFormError;
+};
