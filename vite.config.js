3933e0f4f032fade8d2937f4c02087bc76d401fa
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-10 07:34:34 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:8000/api/v1',
+        target:'https://memome-srv.onrender.com/api/v1',
         changeOrigin: true,
         rewrite:path=>path.replace(/^\/api/, '')
       },
