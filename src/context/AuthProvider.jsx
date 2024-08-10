e6779f1f31a5f94439c0f4752bdad56acb6b44c5
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-10 07:59:04 +0530

    add

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -16,8 +16,7 @@ export default function AuthProvider({ children }) {
             headers: {
               Authorization: `Bearer ${token}`,
               "Content-Type": "application/json",
-            },
-            credentials:'include'
+            }
           });
           const { data } = await res.json();
           setUser(data.user);
