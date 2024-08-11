583442c380c352a39981cf03a939c7deb3afa408
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-11 13:14:23 +0530

    debug

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -19,6 +19,7 @@ export default function AuthProvider({ children }) {
             credentials: "include"
           });
           const { data } = await res.json();
+          console.log(res)
           setUser(data.user);
         } catch (err) {
           console.log(err);
