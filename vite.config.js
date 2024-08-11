e14037a0fd4e63b8b7408c955a941d5ca9565f4d
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-11 14:42:46 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       '/api':{
         target:'https://memome-srv.onrender.com/api/v1',
         changeOrigin: true,
-        rewrite:path=>path.replace(/^\/api/, '')
+        cookieDomainRewrite: ''
       },
     }
   }
