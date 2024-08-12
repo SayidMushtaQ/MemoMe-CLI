f8c2768d14d793cd32b433e502c4dc39ca0cfdbd
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-12 10:38:33 +0530

    add

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
-const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-export {EMAIL_REGEX}
+const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const userExcludePaths = ["/login", "/register", "/verify"];
+export { EMAIL_REGEX, userExcludePaths };
--- src/middleware/authenticateUser.jsx
+++ src/middleware/authenticateUser.jsx
@@ -1,7 +1,9 @@
 import { useAuth } from "../hook/useAuth";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { userExcludePaths } from "../constants";
 export default function AuthenticateUser() {
   const { user, loading } = useAuth();
+  const location = useLocation();
   if (loading) {
     return (
       <div
@@ -18,9 +20,13 @@ export default function AuthenticateUser() {
       </div>
     );
   }
-  if(user && !user.isVerified){
-    localStorage.setItem('user_email',user.email)
-    return <Navigate to={'/sent-verify'}/>
+  if (user && !user.isVerified) {
+    localStorage.setItem("user_email", user.email);
+    return <Navigate to={"/sent-verify"} />;
   }
+  if (user && userExcludePaths.includes(location.pathname) && user.isVerified) {
+    return <Navigate to={"/"} />;
+  }
+
   return user ? <Outlet /> : <Navigate to="/login" />;
 }
